bd6956621e86bcdb483e250149d9d37e8a8a71a0
Veanty <plmulticraftpl@gmail.com>
2023-06-01 15:37:12 +0200

    Feat: move code to seperate .cpp file

---
+++ OthelloReversi/CzlowiekGracz.cpp
@@ -0,0 +1,33 @@
+#include "CzlowiekGracz.h"
+
+void CzlowiekGracz::zrob_ruch(Plansza& plansza) {
+	int rzad;
+	int kolumna;
+	while (true)
+	{
+		if (kolor == Pole::CZARNE)
+		{
+			std::cout << "\n\t\tRuch gracza CZARNE (X)\n";
+		}
+		else
+		{
+			std::cout << "\n\t\tRuch gracza BIALE (O)\n";
+		}
+		std::cout << "\n\t\tPodaj rzad i kolumne: ";
+		std::cin >> rzad >> kolumna;
+		rzad--;
+		kolumna--;
+		if (plansza.sprawdzanie_ruchu(rzad, kolumna, kolor) == 1)
+		{
+			plansza.ruch(rzad, kolumna, kolor);
+			break;
+		}
+		else
+		{
+			plansza.wypisz_plansze();
+			std::cout << "\n\t\tNiepoprawny ruch, sprobuj jeszcze raz!" << std::endl;
+		}
+	}
+
+
+}
