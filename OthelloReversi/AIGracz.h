3003e5221340c00fb4cb098053494b61c76c4cdc
Veanty <plmulticraftpl@gmail.com>
2023-06-01 15:40:48 +0200

    Feat: create new header file for AIGracz class

---
+++ OthelloReversi/AIGracz.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef AIGRACZ_H
+#define AIGRACZ_H
+
+#include "Gracz.h"
+#include "Ruch.h"
+
+class AIGracz : public Gracz {
+public:
+	AIGracz(Pole kolor) : Gracz(kolor) {};
+	void zrob_ruch(Plansza& plansza) override;
+};
+
+
+#endif // !AIGRACZ_H
