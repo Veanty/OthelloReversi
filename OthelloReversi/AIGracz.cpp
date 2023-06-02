c374f1e880caa07b736dd709c3e967b8fa67978a
Veanty <plmulticraftpl@gmail.com>
2023-06-02 02:03:17 +0200

    Fix: now stops playing if no valid move is found

--- OthelloReversi/AIGracz.cpp
+++ OthelloReversi/AIGracz.cpp
@@ -1,15 +1,21 @@
-#include "AIGracz.h"
+﻿#include "AIGracz.h"
 
 void AIGracz::zrob_ruch(Plansza& plansza) {
 
-	Ruch zapamietane_ruchy[20];
+	struct Ruch {
+		int rzad;
+		int kolumna;
+	};
+
+	const int max_ruchy = 20;
+	Ruch zapamietane_ruchy[max_ruchy];
 	int k = 0;
 
 	for (int rzad = 0; rzad < plansza.rozmiar_planszy; rzad++)
 	{
 		for (int kolumna = 0; kolumna < plansza.rozmiar_planszy; kolumna++)
 		{
-			if (plansza.sprawdzanie_ruchu(rzad, kolumna, kolor) == 1)
+			if (plansza.sprawdzanie_ruchu(rzad, kolumna, kolor) == 1 && k < max_ruchy)
 			{
 				zapamietane_ruchy[k].rzad = rzad;
 				zapamietane_ruchy[k].kolumna = kolumna;
@@ -18,19 +24,14 @@ void AIGracz::zrob_ruch(Plansza& plansza) {
 		}
 	}
 
-	int random = rand() % k;
-	while (true)
+	if (k == 0)
 	{
-		if (plansza.sprawdzanie_ruchu(zapamietane_ruchy[random].rzad, zapamietane_ruchy[random].kolumna, kolor) == 1)
-		{
-			plansza.ruch(zapamietane_ruchy[random].rzad, zapamietane_ruchy[random].kolumna, kolor);
-			std::cout << "Komputer: " << zapamietane_ruchy[random].rzad + 1 << " " << zapamietane_ruchy[random].kolumna + 1 << std::endl;
-			break;
-		}
-		else
-		{
-			random = rand() % k;
-		}
+		std::cout << "Komputer: Brak dostępnych ruchow" << std::endl;
+		return;
 	}
 
+	int random = rand() % k;
+
+	plansza.ruch(zapamietane_ruchy[random].rzad, zapamietane_ruchy[random].kolumna, kolor);
+	std::cout << "Komputer: " << zapamietane_ruchy[random].rzad + 1 << " " << zapamietane_ruchy[random].kolumna + 1 << std::endl;
 }
