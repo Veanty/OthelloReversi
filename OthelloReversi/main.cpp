c0bc6bbc7a38a584f342f6e25fe7e9b1eda0a28e
Veanty <plmulticraftpl@gmail.com>
2023-06-02 02:04:06 +0200

    Change: move author meta to intro()

--- OthelloReversi/main.cpp
+++ OthelloReversi/main.cpp
@@ -4,14 +4,9 @@
 #include "AIGracz.h"
 #include "Utils.h"
 
-int ilosc_bialych = 0;
-int ilosc_czarnych = 0;
-
-
 int main() {
 
 	std::srand(std::time(0));
-	std::cout << "Witaj w grze Othello/Reversi by Jakub Drozd" << std::endl;
 	intro();
 
 	return 0;
